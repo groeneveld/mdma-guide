32883f787b01a0125c77a0858c169c4907c0372c
Mark Groeneveld <mgroeneveld@protonmail.ch>
2025-12-15 14:53:16 -0500

    Full editing of efficacy.

--- clear.sh
+++ clear.sh
@@ -13,17 +13,4 @@ rm *.gls
 rm -rf `biber --cache`
 rm "Open MDMA.PDF"
 rm "Open MDMA.tex"
-rm *.4ct
-rm *.4tc
-rm *.css
-rm *.dvi
-rm *.idv
-rm *.lg
-rm *.ncx
-rm *.tmp
-rm *.xhtml
-rm *.xref
-rm *.svg
-rm *.opf
-rm -r Open_MDMA-epub3
 rm paper.pdf
