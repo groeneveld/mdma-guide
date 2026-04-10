5043a5113c3f5d5681db4147465a058eddf94ad9
Mark Groeneveld <mgroeneveld@protonmail.ch>
2026-04-10 08:08:59 -0400

    Rearranged some auxillary files

--- epub.sh
+++ epub.sh
@@ -3,7 +3,7 @@ python3 epub/expand_refs.py
 pandoc \
     --citeproc \
     --bibliography=references.bib \
-    --csl=epub/nature.csl \
+    --csl=auxillaryFiles/nature.csl \
     -f latex -t epub3 \
     -o Open\ MDMA.epub \
     -M link-citations=true \
